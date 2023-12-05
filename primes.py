a9766ee0374b3f27e5da1e10e95298c512218b7e
ChrisHerre334 <chrisherre2003@gmail.com>
2023-12-05 05:39:52 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,29 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    # Exclude invalid values
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+    
     list = []
+    current_number = 2
+
+    while len(list) < number_of_primes:
+        is_prime = True
+
+        # Check if current_number is divisible by any known prime
+        for prime in list:
+            if prime * prime > current_number:
+                break
+            if current_number % prime == 0:
+                is_prime = False
+                break
+
+        # If current_number is prime, add it to the list
+        if is_prime:
+            list.append(current_number)
+
+        # Move to the next number
+        current_number += 1
+
     return list
